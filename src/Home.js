a81855de81be5a8f7de40da35c4be9aaa1576634
Mauro Restuccia <mrestuccia@mac.com>
2017-04-19 19:48:32 -0400

    New header

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import Header from './Header';
 
-const Home = ()=> <div className='well'>Home</div>;
+const Home = ()=> {
+  return (
+    <Header />
+  );
+};
 
 export default Home;
