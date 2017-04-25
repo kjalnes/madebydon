2817fd170dc968187ac97a6f44224de4815bbf96
Mauro Restuccia <mrestuccia@mac.com>
2017-04-25 12:48:22 -0400

    fix for persistance issue #25

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,47 +1,61 @@
 import axios from 'axios';
 import { loadCart } from './cartReducer';
-import  store  from '../store';
+import store from '../store';
 import { LOGIN_SUCCESS, LOGOUT_SUCCESS } from '../constants/';
 import { loginSuccess, logoutSuccess } from '../actions/';
 
-const login = ( credentials ) => {
-    return( dispatcher ) => {
+
+// Load the user from the token
+const loadUser = (token) => {
+    return (dispatcher) => {
+        if (token) {
+            axios.get(`/api/session/${token}`)
+                .then(response => response.data)
+                .then(user => {
+                    dispatcher(loginSuccess(user));
+                    store.dispatch(loadCart(user.orders[0].id));
+                });
+        }
+    };
+};
+
+
+const login = (credentials) => {
+    return (dispatcher) => {
         axios.post('/api/session', credentials)
-        .then( response => response.data )
-        .then( data => {
-            sessionStorage.setItem('token', data.token)
-            axios.get(`/api/session/${data.token}`)
-            .then( response => response.data)
-            .then( user => {
-                console.log('loginSuccess', loginSuccess)
-                // console.log('user from login fn ', user)
-                dispatcher(loginSuccess(user))
-                store.dispatch(loadCart(user.orders[0].id))
+            .then(response => response.data)
+            .then(data => {
+                localStorage.setItem('token', data.token);
+                dispatcher(loadUser(data.token));
             })
-        })
-        .catch( err => console.log(err))
+            .catch(err => console.log(err));
     }
 }
 
 const logout = () => {
     return (dispatcher) => {
-        // sessionStorage.clear()
-        // localStorage.clear();
+        console.assert('localStorage clear');
+        localStorage.clear();
         return dispatcher(logoutSuccess())
     }
 }
 
-const authReducer = (state={}, action) => {
-    switch(action.type) {
+
+const initialState = { session: !!sessionStorage.jwt };
+
+const authReducer = (state = initialState, action) => {
+    console.log('authReducer', state);
+    console.log('token', localStorage.getItem('token'));
+    switch (action.type) {
         case LOGIN_SUCCESS:
             return Object.assign({}, state, { user: action.user })
         case LOGOUT_SUCCESS:
-            return Object.assign({}, state, { user: null})
+            return Object.assign({}, state, { user: null })
     }
     return state
 }
 
-export { login, logout };
+export { login, logout, loadUser };
 export default authReducer;
 
 
