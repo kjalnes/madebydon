1a3d2a78b00d8c284648a0a2e0101ef4a40a16f1
kjalnes <kris.alnes@gmail.com>
2017-04-27 18:30:13 -0400

    closes issue #35

--- src/containers/CartContainer.js
+++ src/containers/CartContainer.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CartProducts from './CartProducts';
 import CartTotals from './CartTotals';
 import { connect } from 'react-redux';
 
-const CartContainer = ({ activeUser }) => (
+const CartContainer = (props) => (
     <div className='container'>
-        { activeUser ?
+        { props.activeUser ?
             <div>
                 <CartProducts />
                 <hr />
@@ -13,20 +13,26 @@ const CartContainer = ({ activeUser }) => (
                     <div className='col-xs-8'>
                     </div>
                     <div className='col-xs-4'>
-                        <CartTotals />
+                        <CartTotals cartTotal={ props.cartTotal } />
                     </div>
                 </div>
             </div>
             : 'Cart is empty'
         }
     </div>
-)
+);
+
+
+const calculateTotal = (cartItems) => {
+    return cartItems.reduce( (total, item) => total + item.product.price *1,0);
+}
+
 
 const mapStateToProps = (state) => (
   {
-    activeUser: state.auth.user
+    activeUser: state.auth.user,
+    cartTotal: calculateTotal(state.cart.cartItems)
   }
 )
-
 
 export default connect(mapStateToProps)(CartContainer);
