19406f2bdbbcd4cbb1f4ea49e98dd971e2a9c135
Mauro Restuccia <mrestuccia@mac.com>
2017-05-08 23:11:07 -0400

    minor fix

--- src/components/products/productdetails.js
+++ src/components/products/productdetails.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 const ProductDetails = ({ selectedProduct, activeUser, addtoCart, deSelectProduct }) => {
 	if (!selectedProduct) return null;
-	console.log(selectedProduct)
 
 	function createMarkup() {
 		return { __html: selectedProduct.productDetails };
@@ -14,15 +13,14 @@ const ProductDetails = ({ selectedProduct, activeUser, addtoCart, deSelectProduc
 				<img onClick={deSelectProduct} src={`./public/assets/images/style_${selectedProduct.id}.jpg`} />
 			</div>
 			<div className="col-md-6">
-				<span className='custom-title-1'>{selectedProduct.name}</span>
+				<span className="custom-title-1">{selectedProduct.name}</span>
 				<p><b>$ {selectedProduct.price}</b></p>
 				<p>{selectedProduct.description}</p>
 				<p><span dangerouslySetInnerHTML={createMarkup()} /></p>
 				<br /><br />
-				<button onClick={() => addtoCart((activeUser) ? activeUser.orders[0].id : 0, selectedProduct, 1)} className='custom-button-1 product-detail'>Add to cart</button>
+				<button onClick={() => addtoCart((activeUser) ? activeUser.orders[0].id : 0, selectedProduct, 1)} className="custom-button-1 product-detail">Add to cart</button>
 			</div>
 		</div>);
 };
-
 
 export default ProductDetails;
