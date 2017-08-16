e9d3b10b95d6af8fad508fac57d2b7857089329a
kjalnes <kris.alnes@gmail.com>
2017-08-16 14:19:54 -0400

    Product detail view disables on icon click instead of img click

--- src/components/products/productdetails.js
+++ src/components/products/productdetails.js
@@ -3,22 +3,27 @@ import React from 'react';
 const ProductDetails = ({ selectedProduct, activeUser, addtoCart, deSelectProduct }) => {
 	if (!selectedProduct) return null;
 
-	function createMarkup() {
-		return { __html: selectedProduct.productDetails };
+	function listHelper(text) {
+		return text.split('\n').map((detail, key) => {
+			return <span key={key}>{detail}<br /></span>
+		})
 	}
 
 	return (
 		<div className="row">
 			<div className="col-md-6">
-				<img onClick={deSelectProduct} src={`./public/assets/images/${selectedProduct.imgURL}`} />
+				<img src={`./public/assets/images/${selectedProduct.imgURL}`} />
 			</div>
-			<div className="col-md-6">
+			<div className="col-md-5">
 				<span className="custom-title-1">{selectedProduct.name}</span>
 				<p><b>$ {selectedProduct.price}</b></p>
 				<p>{selectedProduct.description}</p>
-				<p><span dangerouslySetInnerHTML={createMarkup()} /></p>
+				<p>{ listHelper(selectedProduct.productDetails) }</p>
 				<br /><br />
 				<button onClick={() => addtoCart((activeUser) ? activeUser.orders[0].id : 0, selectedProduct, 1)} className="custom-button-1 product-detail">Add to cart</button>
+			</div>
+			<div className="col-md-1">
+				<img className='x-icon' onClick={deSelectProduct} src={`./public/assets/images/x.ico`} />
 			</div>
 		</div>);
 };
